202d35a82dd96ec3a63b186cdc1ae651994d9b0b
Tc <jackgoodchen2019@gmail.com>
2020-06-08 19:23:19 +0800

    upd

---
+++ project/config_include/params.py
@@ -0,0 +1,27 @@
+
+
+import os
+
+# #腾讯对象存储
+# TX_SECRET_ID = os.getenv("TX_SECRET_ID",None)
+# TX_SECRET_KEY = os.getenv("TX_SECRET_KEY",None)
+
+
+import os
+
+BASEURL = os.getenv("BASEURL","http://localhost:9018")
+VERSION = os.getenv("VERSION","v1")
+APIURL = "{}/{}{}".format(BASEURL,VERSION,"/api")
+CALLBACKURL = "{}{}".format(APIURL,"/order/txPayCallback")
+
+#COS
+
+COS_secret_id = os.getenv("COS_secret_id","")
+COS_secret_key = os.getenv("COS_secret_key","")
+
+#小程序
+WECHAT_APPID = os.getenv("WECHAT_APPID","")
+WECHAT_SECRET = os.getenv("WECHAT_SECRET","")
+WECHAT_PAY_MCHID = os.getenv("WECHAT_PAY_MCHID",None)
+WECHAT_PAY_KEY = os.getenv("WECHAT_PAY_KEY",None)
+WECHAT_PAY_RETURN_KEY = os.getenv("WECHAT_PAY_RETURN_KEY",None)
