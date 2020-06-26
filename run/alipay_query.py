120fbd164a143ad936b64e0958734736af40f9be
Tc <jackgoodchen2019@gmail.com>
2020-06-26 09:40:41 +0800

    upd

--- run/alipay_query.py
+++ run/alipay_query.py
@@ -14,12 +14,15 @@ django.setup()
 
 from app.order.utils import AlipayBase
 from app.order.models import Order
-
+from lib.utils.mytime import UtilTime
 
 
 if __name__=='__main__':
 
-    for item in Order.objects.filter(status='9'):
+    day = UtilTime().string_to_arrow("2020-06-23","YYYY-MM-DD")
+
+
+    for item in Order.objects.filter(status='9',createtime__gte=day.timestamp):
         response  = AlipayBase().query(orderid=item.orderid)
         if response['code'] == '10000' and response['trade_status'] == 'TRADE_SUCCESS':
             print(response)
